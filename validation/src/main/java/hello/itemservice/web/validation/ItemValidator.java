80ea06b1852653cf2a17540e9bb17b806cf9df6a
Kim Taekyung <seaking7@gmail.com>
2022-03-30 20:58:32 +0900

    Validator를 상속받은 ItemValidator를 빈으로 등록하고 검증하도록

---
+++ validation/src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -0,0 +1,43 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.Validator;
+
+@Component
+public class ItemValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Item.class.isAssignableFrom(clazz);
+        //item == clazz
+        //item == subItem
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Item item = (Item) target;
+
+        if(!StringUtils.hasText(item.getItemName())){
+            errors.rejectValue("itemName", "required", "상품 이름 디폴트 에러메시지." );
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            errors.rejectValue("price", "range", new Object[]{1000, 1000000}, "가격은 디폴트 에러메시지.");
+        }
+        if(item.getQuantity() == null || item.getQuantity() >= 9999 ){
+            errors.rejectValue("quantity", "max", new Object[]{9999},  "수량 디폴트 에러메시지.");
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null ){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, "토탈금액 디폴트 에러메시지");
+            }
+        }
+
+    }
+}
--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,7 +26,9 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
+    
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -151,7 +154,7 @@ public class ValidationItemControllerV2 {
     }
 
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         log.info("object Name = {}", bindingResult.getObjectName());
@@ -192,6 +195,24 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        itemValidator.validate(item, bindingResult);
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
